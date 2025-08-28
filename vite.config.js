e7deab993e89fd23f52037678b3221b34c37c4e3
anhthu258 <k.anhthu02@gmail.com>
2025-08-28 15:00:58 +0200

    config ændring

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/"
+  };
+
+if (command !== "serve") {
+  configs.base = "/spa/";
+}
+
+return configs;
+});
